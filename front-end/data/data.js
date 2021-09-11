2887fd4cf6cdd420b3380a61750f1d9b1b3fead7
Manoj Choudhury <aj.manoj99@gmail.com>
2021-09-11 20:30:01 +0000

    Added Debugger and To String

--- front-end/data/data.js
+++ front-end/data/data.js
@@ -1,6 +1,10 @@
 import * as alaSQLSpace from '../lib/alasql/dist/alasql.js';
 
+import Debugger from "../lib/debugger.js";
 class Data{
+    constructor(){        
+        this.debugger=Debugger(this.debug||this.constructor.debug,this.constructor.name)
+    }
     eventListeners={
 
     }
@@ -25,36 +29,11 @@ class Data{
             });
         }
     }
-
-}
-
-class SQL extends Data {
-    static createTable() {
-        alasql("CREATE TABLE example1 (a INT, b INT)");
-
-        // alasql's data store for a table can be assigned directly
-        alasql.tables.example1.data = [
-            { a: 2, b: 6 },
-            { a: 3, b: 4 }
-        ];
-
-        // ... or manipulated with normal SQL
-        alasql("INSERT INTO example1 VALUES (1,5)");
-
-        var res = alasql("SELECT * FROM example1 ORDER BY b DESC");
-
-        console.log(res); // [{a:2,b:6},{a:1,b:5},{a:3,b:4}]
-
-        alasql('CREATE localStorage DATABASE IF NOT EXISTS Atlas');
-        alasql('ATTACH localStorage DATABASE Atlas AS MyAtlas');
-        alasql('CREATE TABLE IF NOT EXISTS MyAtlas.City (city string, population number)');
-        alasql('SELECT * INTO MyAtlas.City FROM ?', [[
-            { city: 'Vienna', population: 1731000 },
-            { city: 'Budapest', population: 1728000 }
-        ]]);
-        var res = alasql('SELECT * FROM MyAtlas.City');
-        console.log(res);
+    toString(){
+        return this.constructor.name
     }
+
 }
 
-export {Data,SQL};
+
+export {Data};
